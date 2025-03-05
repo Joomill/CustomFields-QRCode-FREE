5db36c163e85116e83a37c4e41c57a8e9d701f8f
Jeroen <info@joomill.nl>
2025-03-05 13:51:35 +0100

    v5.1.0 - (un)install message

--- script.php
+++ script.php
@@ -32,7 +32,6 @@ class plgFieldsQrcodeInstallerScript
 	 */
 	private $minimumPHPVersion = JOOMLA_MINIMUM_PHP;
 
-
 	/**
 	 * Function called before extension installation/update/removal procedure commences
 	 *
@@ -71,6 +70,57 @@ class plgFieldsQrcodeInstallerScript
 			}
 		}
 
+		return true;
+	}
+
+	/**
+	 * Function called after extension installation/update/removal procedure commences
+	 *
+	 * @param string $type The type of change (install, update or discover_install, not uninstall)
+	 * @param InstallerAdapter $parent The class calling this method
+	 * @return  boolean  True on success
+	 * @since  4.0.0
+	 */
+	public function postflight($type, $parent)
+	{
+		if ($type === 'install')
+		{
+			echo '<style>a[target="_blank"]::before {display: none;}</style>';
+			echo '<div class="mb-3 text-center"><img src="https://www.joomill-extensions.com/images/joomill-logo.png" alt="Joomill Extensions" /></div>';
+			echo '<br>';
+			echo '<h3 class="text-center">' . Text::_('PLG_FIELDS_QRCODE_THANKYOU') . '</h3>';
+			echo '<br>';
+			echo '<div class="text-center">' . Text::_('PLG_FIELDS_QRCODE_FOLLOWME') . ':</div>';
+			echo '<div class="text-center">';
+			echo '<a class="m-2" href="https://www.linkedin.com/in/jeroenmoolenschot/" target="_blank"><i class="fa-brands fa-linkedin"> </i></a>';
+			echo '<a class="m-2" href="https://www.facebook.com/Joomill" target="_blank"><i class="fa-brands fa-facebook-f"> </i></a>';
+			echo '<a class="m-2" href="https://www.instagram.com/Joomill" target="_blank"><i class="fa-brands fa-instagram"> </i></a>';
+			echo '<a class="m-2" href="https://bsky.app/profile/joomill.bsky.social" target="_blank"><i class="fa-brands fa-bluesky"> </i></a>';
+			echo '<a class="m-2" href="https://joomla.social/@joomill" target="_blank"><i class="fa-brands fa-mastodon"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.threads.net/@joomill" target="_blank"><i class="fa-brands fa-threads"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.twitter.com/Joomill" target="_blank"><i class="fa-brands fa-brands fa-x-twitter"> </i></a>';
+			echo '<a class="m-2" href="https://community.joomla.org/service-providers-directory/listings/67:joomill.html" target="_blank"><i class="fa-brands fa-joomla"> </i></a>';
+			echo '</div>';
+		}
+		if ($type === 'uninstall')
+		{
+			echo '<style>a[target="_blank"]::before {display: none};</style>';
+			echo '<div class="mb-3 text-center"><img src="https://www.joomill-extensions.com/images/joomill-logo.png" alt="Joomill Extensions" /></div>';
+			echo '<br>';
+			echo '<h3 class="text-center">' . Text::_('PLG_FIELDS_QRCODE_THANKYOU') . '</h3>';
+			echo '<br>';
+			echo '<div class="text-center">' . Text::_('PLG_FIELDS_QRCODE_FOLLOWME') . ':</div>';
+			echo '<div class="text-center">';
+			echo '<a class="m-2" href="https://www.linkedin.com/in/jeroenmoolenschot/" target="_blank"><i class="fa-brands fa-linkedin"> </i></a>';
+			echo '<a class="m-2" href="https://www.facebook.com/Joomill" target="_blank"><i class="fa-brands fa-facebook-f"> </i></a>';
+			echo '<a class="m-2" href="https://www.instagram.com/Joomill" target="_blank"><i class="fa-brands fa-instagram"> </i></a>';
+			echo '<a class="m-2" href="https://bsky.app/profile/joomill.bsky.social" target="_blank"><i class="fa-brands fa-bluesky"> </i></a>';
+			echo '<a class="m-2" href="https://joomla.social/@joomill" target="_blank"><i class="fa-brands fa-mastodon"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.threads.net/@joomill" target="_blank"><i class="fa-brands fa-threads"></i> </i></a>';
+			echo '<a class="m-2" href="https://www.twitter.com/Joomill" target="_blank"><i class="fa-brands fa-brands fa-x-twitter"> </i></a>';
+			echo '<a class="m-2" href="https://community.joomla.org/service-providers-directory/listings/67:joomill.html" target="_blank"><i class="fa-brands fa-joomla"> </i></a>';
+			echo '</div>';
+		}
 		return true;
 	}
 
